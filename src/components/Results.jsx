06e1db2de439b0c37db20684b6b63532d100fc7e
bsarnii <scharnoui@gmail.com>
2022-09-21 18:19:28 +0200

    updated loading element

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -6,26 +6,28 @@ const Results = () => {
     const dataValue= useContext(DataContext)
 
   return (
+    <>
+    {dataValue.display === false ? "" : 
     <section className="results">
-        {dataValue.display === false ? 
-        <div className="lds-dual-ring"></div>
-        : 
-        <><div className="ip-address">
-                      <small>IP ADDRESS</small>
-                      <h1>{dataValue.ip}</h1>
-                  </div><div className="location">
-                          <small>LOCATION</small>
-                          <h1>{dataValue.city}, {dataValue.region}</h1>
-                      </div><div className="timezone">
-                          <small>TIMEZONE</small>
-                          <h1>UTC {dataValue.timezone}</h1>
-                      </div><div className="isp">
-                          <small>ISP</small>
-                          <h1>{dataValue.isp}</h1>
-                      </div>
-        </>
-    }
+        <div className="ip-address">
+            <small>IP ADDRESS</small>
+            <h1>{dataValue.ip}</h1>
+            </div>
+            <div className="location">
+            <small>LOCATION</small>
+            <h1>{dataValue.city}, {dataValue.region}</h1>
+            </div>
+            <div className="timezone">
+            <small>TIMEZONE</small>
+            <h1>UTC {dataValue.timezone}</h1>
+            </div>
+            <div className="isp">
+            <small>ISP</small>
+            <h1>{dataValue.isp}</h1>
+            </div>
     </section>
+    }
+    </>
   )
 }
 
